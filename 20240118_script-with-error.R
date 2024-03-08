5ac1face589a511fd004f35fb2f2ab5120af37d7
Raman Sran <Raman.Sran@dpird.wa.gov.au>
2024-03-08 17:24:20 +0800

    I corrected your calculation, Nielsen.

--- 20240118_script-with-error.R
+++ 20240118_script-with-error.R
@@ -4,6 +4,6 @@ a_value = 10
 another_value = 5
 
 # There is something wrong with this calculation
-add_values = a_value * another_value 
+add_values = a_value + another_value 
 
 stopifnot("The addition of 10 and 5 should be 15" = add_values == 15)
